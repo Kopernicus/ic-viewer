b0eff7d5d1b82d5624dc3d9cd5d5aac690a8dd40
Dorian Stoll <dorian.stoll@tmsp.io>
2018-08-11 05:20:29 +0200

    Allow compiling the project using the commandline

---
+++ Unity Project/IC Viewer/Assets/Editor/WebGLBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class WebGLBuilder 
+{
+	/// <summary>
+	/// Allows us to compile the project using the commandline
+	/// </summary>
+	public static void Build() 
+	{
+		const String pathToDeploy = "build/";
+		
+		// Collect all scenes that should get built
+		String[] scenes = new String[SceneManager.sceneCountInBuildSettings];
+		for (Int32 i = 0; i < scenes.Length; i++)
+		{
+			scenes[i] = SceneManager.GetSceneByBuildIndex(i).path;
+		}
+
+		BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.WebGL, BuildOptions.None);      
+	}
+}
