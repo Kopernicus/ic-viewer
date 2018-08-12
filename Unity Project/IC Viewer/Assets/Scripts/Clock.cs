2c6a8ba99835573d1188f2b9d3f3197709e79565
Dorian Stoll <dorian.stoll@tmsp.io>
2018-08-12 15:21:46 +0200

    idk

---
+++ Unity Project/IC Viewer/Assets/Scripts/Clock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+	private Text _text;
+
+	public String Format = "HH : mm : ss";
+	
+	void Start ()
+	{
+		_text = GetComponent<Text>();
+	}
+	
+	void Update ()
+	{
+		_text.text = DateTime.Now.ToString(Format);
+	}
+}
