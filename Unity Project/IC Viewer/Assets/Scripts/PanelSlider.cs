676806bb047f228bd4ce2693958cd43090af961d
Dorian Stoll <dorian.stoll@tmsp.io>
2018-08-13 00:46:07 +0200

    Add the ability to fade out the UI

---
+++ Unity Project/IC Viewer/Assets/Scripts/PanelSlider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class PanelSlider : MonoBehaviour
+{
+	public Single EnabledSpeed;
+	public Single DisabledSpeed;
+
+	public GameObject EnabledPanel;
+	public GameObject DisabledPanel;
+
+	private Vector3 _enabledPosition;
+	private Vector3 _disabledPostion;
+
+	void Start()
+	{
+		_enabledPosition = EnabledPanel.transform.position;
+		_disabledPostion = DisabledPanel.transform.position;
+	}
+	
+	public void TogglePanel()
+	{
+		_enabledPosition = DisabledPanel.transform.position;
+		_disabledPostion = EnabledPanel.transform.position;
+	}
+	
+	void Update () 
+	{
+		Debug.Log("1: " + EnabledPanel.transform.position);
+		Debug.Log("2: " + _enabledPosition);
+		Debug.Log("3: " + DisabledPanel.transform.position);
+		Debug.Log("4: " + _disabledPostion);
+		if (!(EnabledPanel.transform.position == _enabledPosition))
+		{
+			EnabledPanel.transform.position = Vector3.Lerp(EnabledPanel.transform.position, _enabledPosition,
+				EnabledSpeed * Time.deltaTime);
+		}
+
+		if (!(DisabledPanel.transform.position == _disabledPostion))
+		{
+			DisabledPanel.transform.position = Vector3.Lerp(DisabledPanel.transform.position, _disabledPostion,
+				DisabledSpeed * Time.deltaTime);
+		}
+	}
+}
