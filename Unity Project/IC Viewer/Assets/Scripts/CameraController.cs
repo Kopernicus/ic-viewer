638d9318da6811a2b71c5c3f65a15178d180c401
cwongmath <cwongmath@gmail.com>
2018-08-12 15:43:52 -0500

    Zoom
    This adds zoom. Credit to JelleWho at github.com/jellewie.

--- Unity Project/IC Viewer/Assets/Scripts/CameraController.cs
+++ Unity Project/IC Viewer/Assets/Scripts/CameraController.cs
@@ -106,6 +106,22 @@ public class CameraController : MonoBehaviour
 
         centerPoint.y = planeLevel;
         
-        gameObject.transform.Translate(-gameObject.transform.forward * scrl * Time.deltaTime * scrollSensivity);
+        //gameObject.transform.Translate(-gameObject.transform.forward * scrl * Time.deltaTime * scrollSensivity);
+
+		float ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");           //This little peece of code is written by JelleWho https://github.com/jellewie
+		if (ScrollWheelChange != 0){                                            //If the scrollwheel has changed
+			float R = ScrollWheelChange * 15;                                   //The radius from current camera
+			float PosX = Camera.main.transform.eulerAngles.x + 90;              //Get up and down
+			float PosY = -1 * (Camera.main.transform.eulerAngles.y - 90);       //Get left to right
+			PosX = PosX / 180 * Mathf.PI;                                       //Convert from degrees to radians
+			PosY = PosY / 180 * Mathf.PI;                                       //^
+			float X = R * Mathf.Sin(PosX) * Mathf.Cos(PosY);                    //Calculate new coords
+			float Z = R * Mathf.Sin(PosX) * Mathf.Sin(PosY);                    //^
+			float Y = R * Mathf.Cos(PosX);                                      //^
+			float CamX = Camera.main.transform.position.x;                      //Get current camera postition for the offset
+			float CamY = Camera.main.transform.position.y;                      //^
+			float CamZ = Camera.main.transform.position.z;                      //^
+			Camera.main.transform.position = new Vector3(CamX + X, CamY + Y, CamZ + Z);//Move the main camera
+		}
     }
 }
